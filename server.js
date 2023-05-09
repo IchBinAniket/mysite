d99dc85ee48012fc9a80e9893e4452cf5183c981
aniket7774 <69353704+aniket7774@users.noreply.github.com>
2023-05-09 19:38:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('/', function (req, res) {
 
 
 app.get('/hello', function (req, res) {
-    var person={firstName:'Ravi',lastName:'Tambade',age:47};
+    var person={firstName:'Aniket',lastName:'Bali',age:22};
     res.send(person);
 });
 
